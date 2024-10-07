6e50514db0997a7a85aa474634ac9d0dac48c1d3
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-10-07 19:07:55 +0530

    working, ready for deployment

---
+++ main.py
@@ -0,0 +1,58 @@
+import re
+import json
+import streamlit as st
+
+def convert_chat_to_clean_json(chat_content):
+    try:
+        dateRegex = r"[\d]{1,2}/[\d]{1,2}/[\d]{2}"  
+        timeRegex = r"[\d]{1,2}:\d{2}\s*(?:[aApP][mM])" 
+        messageRegex = dateRegex + r', ' + timeRegex + r' - (.*?): (.*)'
+
+        messages = []
+
+        for line in chat_content.splitlines():
+            line = line.strip()  
+            message_match = re.match(messageRegex, line)
+            if message_match:
+                sender = message_match.group(1)
+                text = message_match.group(2)
+                messages.append({"sender": sender, "message": text})
+             # Add to previous message if continuation
+            elif messages: 
+                # Handle multi-line messages
+                messages[-1]["message"] += '\n' + line  
+
+        return messages
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        return None
+
+def main():
+    st.title("WhatsApp Chat Converter")
+    
+    chat_file = st.file_uploader("Upload your WhatsApp Chat (.txt)", type=["txt"])
+    
+    if chat_file is not None:
+        chat_content = chat_file.read().decode('utf-8')
+        
+        messages = convert_chat_to_clean_json(chat_content)
+        
+        if messages:
+            st.success("Chat successfully converted!")
+            
+            # # Display the extracted messages
+            # st.write("Extracted Messages:")
+            # st.json(messages)
+            
+            # Option to download JSON
+            json_data = json.dumps(messages, indent=3)
+            st.download_button(
+                label="Download JSON",
+                data=json_data,
+                file_name='chat_clean.json',
+                mime='application/json'
+            )
+
+if __name__ == '__main__':
+    main()
